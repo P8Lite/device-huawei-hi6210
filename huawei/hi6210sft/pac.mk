fd09a690c861d686006a3c38200db97fcd999c44
P8Lite <audriusleague@gmail.com>
2016-10-11 09:16:23 +0300

    updated config

---
+++ huawei/hi6210sft/pac.mk
@@ -0,0 +1,13 @@
+#  Cyanogen Mod
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Device
+$(call inherit-product, device/huawei/hi6210sft/full_hi6210sft.mk)
+
+PRODUCT_DEVICE := hi6210sft
+PRODUCT_NAME := pac_hi6210sft
+PRODUCT_BRAND := huawei
+PRODUCT_MODEL := ALE-L21
+PRODUCT_MANUFACTURER := HUAWEI
+
